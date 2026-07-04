422695d4d1a11b974131c8865e4a0576818f354d
Vedant16Pawar <vedant16pawar@gmail.com>
2026-07-04 18:23:10 +0530

    Initial commit

---
+++ practice_ulr.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# print("Loaded MONGODB_URL:", os.getenv("MONGODB_URL"))
+
+import sklearn
+# print("TRAINING sklearn version:", sklearn.__version__)
+
+
