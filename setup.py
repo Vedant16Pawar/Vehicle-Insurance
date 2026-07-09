c6f49bd2a6101785dc26bb4b0b1e98d854eefba7
Vedant16Pawar <vedant16pawar@gmail.com>
2026-07-09 11:13:15 +0530

    Final commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Vinit",
-    author_email="joshivinit57@gmail.com",
+    author="Vedant",
+    author_email="vedant16pawar@gmail.com",
     packages=find_packages()
 )
